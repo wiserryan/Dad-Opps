1773da621b5a2db0ceef0b4359fd38a67adda9aa
Ryan Wiser <wiserryan@gmail.com>
2023-06-06 01:31:50 -0500

    images uploaded

---
+++ server/modules/gallery.data.js
@@ -0,0 +1,9 @@
+const galleryItems = [
+    { id: 1, path: 'images/Boyd_park.png', description: 'Awesome play area with lots of space, picnic tables, and feels safe.', likes: 0 },
+    { id: 1, path: 'images/Edgcumbe_rec.png', description: 'Huge playground play with softball field near by, benches, and rec center nearby.', likes: 0 },
+    { id: 1, path: 'images/Groveland.png', description: 'Two separate playgrounds in front of school, benches, fairly busy.', likes: 0 },
+    { id: 1, path: 'images/Mattocks_park2.png', description: 'One of our favorite play areas with lots of people, picnic tables,feels safe with field nearby.', likes: 0 },
+
+];
+
+module.exports = galleryItems;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -52,3 +52,4 @@ function LandingPage() {
 }
 
 export default LandingPage;
+
